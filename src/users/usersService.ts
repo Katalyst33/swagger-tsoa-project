5e464087c308d6ab7211078a561b4d262748dda2
sam.nmeje <sam@katalyst.tech>
2024-07-18 05:23:40 +0100

    Update usersService.ts

--- src/users/usersService.ts
+++ src/users/usersService.ts
@@ -1,5 +1,5 @@
 // src/users/usersService.ts
-import { User } from "./user";
+import {User} from "./user";
 
 // A post request should not contain an id.
 export type UserCreationParams = Pick<User, "email" | "name" | "phoneNumbers">;
